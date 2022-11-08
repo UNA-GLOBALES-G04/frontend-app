c9402177fa88271f4f3e82c50323a116018d2c65
Tidael <andressalas0160@gmail.com>
2022-11-07 21:16:47 -0600

    f

--- src/modules/navbar/components/DesktopNav.js
+++ src/modules/navbar/components/DesktopNav.js
@@ -24,46 +24,48 @@ import {
 
 import { DesktopSubNav } from './';
 
-const NAV_ITEMS = [
-  {
-    label: 'Inspiration',
-    children: [
-      {
-        label: 'Explore Design Work',
-        subLabel: 'Trending Design to inspire you',
-        href: '#',
-      },
-      {
-        label: 'New & Noteworthy',
-        subLabel: 'Up-and-coming Designers',
-        href: '#',
-      },
-    ],
-  },
-  {
-    label: 'Find Work',
-    children: [
-      {
-        label: 'Job Board',
-        subLabel: 'Find your dream design job',
-        href: '#',
-      },
-      {
-        label: 'Freelance Projects',
-        subLabel: 'An exclusive list for contract work',
-        href: '#',
-      },
-    ],
-  },
-  {
-    label: 'Learn Design',
-    href: '#',
-  },
-  {
-    label: 'Hire Designers',
-    href: '#',
-  },
-];
+// const NAV_ITEMS = [
+//   {
+//     label: 'Inspiration',
+//     children: [
+//       {
+//         label: 'Explore Design Work',
+//         subLabel: 'Trending Design to inspire you',
+//         href: '#',
+//       },
+//       {
+//         label: 'New & Noteworthy',
+//         subLabel: 'Up-and-coming Designers',
+//         href: '#',
+//       },
+//     ],
+//   },
+//   {
+//     label: 'Find Work',
+//     children: [
+//       {
+//         label: 'Job Board',
+//         subLabel: 'Find your dream design job',
+//         href: '#',
+//       },
+//       {
+//         label: 'Freelance Projects',
+//         subLabel: 'An exclusive list for contract work',
+//         href: '#',
+//       },
+//     ],
+//   },
+//   {
+//     label: 'Learn Design',
+//     href: '#',
+//   },
+//   {
+//     label: 'Hire Designers',
+//     href: '#',
+//   },
+// ];
+
+const NAV_ITEMS = []
 
 const DesktopNav = () => {
   const linkColor = useColorModeValue('gray.600', 'gray.200');
